8e62fdbb94fef67738c80fbbd211b74947c9b86e
Dilmurod <142794823+d1ilmurod@users.noreply.github.com>
2024-07-01 18:16:39 +0500

    makemigrations va migrate berildi

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR/'static']
 STATIC_ROOT = BASE_DIR/'staticfiles'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
